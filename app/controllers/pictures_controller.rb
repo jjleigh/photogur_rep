7002b471c186fcd7752af468d300667bfa04af23
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-17 18:15:43 -0400

    updated the new and update actions in PicturesController and updated new view with form_for helper

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,13 +7,25 @@ class PicturesController < ApplicationController
 def show
     @picture = @pictures[params[:id].to_i]
   end
-  
+
   def new
+  	@picture = Picture.new
   end
 
   def create 
-  	render :text => "saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  	@picture = Picture.create(picture_params)
+  	if @picture.save
+  		redirect_to pictures_url
+  	else
+  		render :new
+  	end
   end 
+
+  private 
+  def picture_params
+  	params.require(:picture).permit(:artist, :title, :url)
+  end
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Photogur::Application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
-  get 'pictures/new' => 'pictures#new'
-  post 'pictures/' => 'pictures#create'
-  get 'pictures/:id' => 'pictures#show', as: "picture"
+  get '/pictures/new' => 'pictures#new'
+  post '/pictures/' => 'pictures#create'
+  get '/pictures/:id' => 'pictures#show', as: "picture"
   
 
 
