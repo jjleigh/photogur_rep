a67cb42d1525cd0454857dde297a322ffc89a38d
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-17 19:14:46 -0400

    removed routes and replace with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Photogur::Application.routes.draw do
 
-  root :to => "pictures#index"
+  # root :to => "pictures#index"
 
-  get 'pictures' => 'pictures#index'
+  # get 'pictures' => 'pictures#index'
 
-  get '/pictures/new' => 'pictures#new'
-  post '/pictures/' => 'pictures#create'
-  get '/pictures/:id' => 'pictures#show', as: "picture"
+  # get '/pictures/new' => 'pictures#new'
+  # post '/pictures/' => 'pictures#create'
+  # get '/pictures/:id' => 'pictures#show', as: "picture"
   
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
+  # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # patch 'pictures/:id' => "pictures#update"
 
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+  # delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 
+  resources :pictures
   root :to => "pictures#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
