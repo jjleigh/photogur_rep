d6c594df527fffd46cf2ac725542bca50de51893
Justine Jones <justinejones91@gmail.com>
2014-08-14 11:47:24 -0400

    app/controllers/comments_controller.rb '

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,40 @@
 class CommentsController < ApplicationController
   def new
-    
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
 
+    if @comment.save
+      redirect_to 
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update_attributes(comment_params)
+      redirect_to
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+     @comment = Comment.find(params[:id])
+
+     @comment.destroy
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:description)
+    end
 end
