261d224e4e6ff11336f8c3f3661388586606dc26
Justine Jones <justinejones91@gmail.com>
2014-08-14 12:17:41 -0400

    added the fields to be permitted in the user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-    params.require(user)
+    params.require(user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
