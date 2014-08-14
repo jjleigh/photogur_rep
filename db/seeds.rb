de0767ac94c90a48fc535df32e88e3e545859ae3
Justine Jones <justinejones91@gmail.com>
2014-08-14 15:40:43 -0400

    added some fake pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,5 +21,46 @@ Picture.create!(
 	:title  => "Into the Poppies",
     :artist => "John Wilhelm",
     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg" 
+
 	)
 
+
+
+
+
+Picture.create!(
+	:title  => "Into The Blue",
+    :artist => "Naomi campbell",
+    :url    => "http://media-cache-ec0.pinimg.com/736x/db/97/12/db9712b331de1a20bb969222e2b4187e.jpg"
+	)
+Picture.create!(
+	:title  => "Rocky point",
+    :artist => "Naom Chompsky",
+    :url    => "http://media-cache-ec0.pinimg.com/736x/ad/9e/53/ad9e53cf042f03cd6ccda739123519b8.jpg"
+	)
+
+Picture.create!(
+	:title  => "Personal trainer",
+    :artist => "Joan Smalls",
+    :url    => "http://media-cache-ak0.pinimg.com/736x/6b/5d/63/6b5d63b54a8890691536eb25135c2000.jpg"
+	)
+
+
+
+
+Picture.create!(
+	:title  => "Girl with the fro",
+    :artist => "Bob Marley",
+    :url    => "http://media-cache-ak0.pinimg.com/736x/27/7d/5a/277d5a4f87ae6487916f5ced9f787dab.jpg"
+	)
+Picture.create!(
+	:title  => "Venom",
+    :artist => "Christopher Hitchens",
+    :url    => "http://media-cache-ak0.pinimg.com/736x/26/ce/ff/26ceffe529817bef44d67815deec196b.jpg"
+	)
+
+Picture.create!(
+	:title  => "Sonata",
+    :artist => "Shaba Ranks",
+    :url    => "http://media-cache-ak0.pinimg.com/736x/6c/28/81/6c2881cc336885876758a755cd918dde.jpg"
+	)
