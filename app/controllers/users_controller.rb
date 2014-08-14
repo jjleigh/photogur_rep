c7b1f430bc32fe690b4fefc25be04f0c38843356
Justine Jones <justinejones91@gmail.com>
2014-08-14 11:46:59 -0400

    added the actions to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,51 @@
 class UsersController < ApplicationController
   def index
+    @users= Users.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to 
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
+
   def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
   end
+
+  private
+
+    def user_params
+    params.require(user)
+    end
+
 end
