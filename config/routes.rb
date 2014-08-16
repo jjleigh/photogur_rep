0833bf18b569066ae7d1eb714048ba7a21026529
Justine Jones <justinejones91@gmail.com>
2014-08-16 01:09:34 -0400

    added the session routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Photogur::Application.routes.draw do
   resources :pictures
   root :to => "pictures#index"
 
+  resources :sessions, :only => [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
