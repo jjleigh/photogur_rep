6b6adef3e053fd03f6eb8bd99198563e44454c08
Justine Jones <justinejones91@gmail.com>
2014-08-16 01:02:16 -0400

    added has secure password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to 
+      redirect_to pictures_url, :notice => "Thanks for signing up!"
     else
       render 'new'
     end
