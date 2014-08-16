3bc6465b3c949ca7b5f38c18abf84125187140c1
Justine Jones <justinejones91@gmail.com>
2014-08-16 03:11:43 -0400

    changed the products url mistake to pictures url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
 class SessionsController < ApplicationController
+ skip_before_filter :require_login, only: [:index, :new, :create]
+
   def new
   end
 
   def create
-  	user =User.find_by_email(params[:email])
-
-  	if user && user.authenticate(params[:password])
-  		session[:user_id] = user.user_id
-  		redirect_to pictures_url
-  	else 
-  		flash.now[:alert] = "Invalid email or password"
-  		render :new
-  	end
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to pictures_url, :notice => "Logged in!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render "new"
+    end
   end
 
   def destroy
