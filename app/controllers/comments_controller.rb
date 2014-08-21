8e5536bb3315a59d0eef9c87a147c4a5403b2620
Justine Jones <justinejones91@gmail.com>
2014-08-21 18:13:08 -0400

    got comments to work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,27 @@
 class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
+   before_filter :ensure_logged_in, :only => [:edit, :create, :show, :update, :destroy]
+   before_filter :load_picture
+
+  def show
+    @comment = Comment.find(params[:id])
   end
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @picture.comments.build(comment_params)
+    @comment.user_id =current_user.id
 
-    if @comment.save
-      redirect_to 
-    else
-      render 'new'
+    respond_to do |format|
+      if @comment.save
+        format.html {redirect_to picture_path(@picture.id)}
+        format.js {} #This will look for app/views/reviews/create.js.erb
+      else
+        format.html {render 'pictures/show', alert: "There was an error."}
+        format.js {}
+      end
     end
   end
+
+
 
   def edit
     @comment = Comment.find(params[:id])
@@ -29,12 +39,17 @@ class CommentsController < ApplicationController
 
   def destroy
      @comment = Comment.find(params[:id])
-
      @comment.destroy
   end
 
+
   private
-    def comment_params
-      params.require(:comment).permit(:description)
-    end
+  def comment_params
+    params.require(:comment).permit(:description, :picture_id, :user_id)
+  end
+
+  def load_picture
+    @picture = Picture.find(params[:picture_id])
+  end
+
 end
