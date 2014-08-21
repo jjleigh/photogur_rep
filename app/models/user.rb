5ca5349c5367268794df7fe9a4698f3f1567c95f
Justine Jones <justinejones91@gmail.com>
2014-08-21 18:18:05 -0400

    added has many through association to connect users to comments and pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
-	has_many :pictures
+	has_many :pictures, :through => :comments
 	has_many :comments
 
 	validates_presence_of :name , :emal, :password
