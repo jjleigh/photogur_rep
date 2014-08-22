1ebe398e4c57969bd01cdbbdf1f24a651c7a8d40
Justine Jones <justinejones91@gmail.com>
2014-08-21 20:36:09 -0400

    removed the logged in and out messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to pictures_url, :notice => "Logged in!"
+      redirect_to pictures_url
     else
       flash.now[:alert] = "Invalid email or password"
       render "new"
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
   	session[:user_id] = nil
 
-  	redirect_to pictures_url, :notice => "Logged out!"
+  	redirect_to pictures_url
   end
 end
