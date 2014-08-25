2232ff5a05d72924dccfc29ca9f12a2e32d4494a
Justine Jones <justinejones91@gmail.com>
2014-08-25 18:55:38 -0400

    added validations to the comments for length and presence

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
 	belongs_to :picture
 	belongs_to :user
+
+	validates :description, presence: true
+	validates :description, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed"}
 end
