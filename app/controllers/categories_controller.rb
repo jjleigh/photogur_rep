93a28887398671b443e33a29072e3821e8ff13e5
Justine Jones <justinejones91@gmail.com>
2014-08-25 13:23:07 -0400

    got the images to display

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
 	
 	def show
 		@categories = Category.all
-		@Category = Category.find(params[:id])
+		@category = Category.find(params[:id])
 		@pictures_in_a_category = @category.pictures	
 	end
 
