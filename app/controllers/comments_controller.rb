5c8edb9d163ef4b24ef5a74994ca07a656b35f65
Justine Jones <justinejones91@gmail.com>
2014-08-25 23:02:05 -0400

    added a redirect_to for the destroy method and fixed the delete link for the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,6 +40,8 @@ class CommentsController < ApplicationController
   def destroy
      @comment = Comment.find(params[:id])
      @comment.destroy
+
+     redirect_to picture_path(@picture)
   end
 
 
