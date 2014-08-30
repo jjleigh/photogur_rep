2c445bd1466fda08bc6f5e31b2e694e99a000376
Justine Jones <justinejones91@gmail.com>
2014-08-29 21:57:07 -0400

    set the default scope to put the newest pictures first

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
-	scope :newest_first, -> { order("created_at DESC")}
+	default_scope { order("created_at DESC")}
 	scope :most_recent_nive, -> { newest_first.limit(9)}
 	scope :created_before, ->(time) { where("created_at < ?", time)}
 
