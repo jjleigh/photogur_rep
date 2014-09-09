6675f9d99a7fe39576ce59f806e6f9f429cf25c6
Justine Jones <justinejones91@gmail.com>
2014-09-09 09:23:26 -0400

    fixed the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,47 +11,47 @@
 # this will seed the database wth categories
 	Category.create!(
 		:name => "Landscape",
-		:cat_image_url => "assets/landscape.jpg"
+		:cat_image_url => "assets/landscape.jpg",
 		:id => 1
 		)
 
 	Category.create!(
 		:name => "Portrait",
-		:cat_image_url => "assets/portrait.jpg"
+		:cat_image_url => "assets/portrait.jpg",
 		:id => 2
 		)
 	Category.create!(
 		:name => "Meme",
-		:cat_image_url => "assets/meme.jpg"
+		:cat_image_url => "assets/meme.jpg",
 		:id => 3
 		)
 	Category.create!(
 		:name => "Still-life",
-		:cat_image_url => "assets/still-life.jpg"
+		:cat_image_url => "assets/still-life.jpg",
 		:id => 4
 		)
 	Category.create!(
 		:name => "Food",
-		:cat_image_url => "assets/food-photogur.jpg"
+		:cat_image_url => "assets/food-photogur.jpg",
 		:id => 5
 		)
 	Category.create!(
 		:name => "Animals",
-		:cat_image_url => "assets/animals-photogur.jpg"
+		:cat_image_url => "assets/animals-photogur.jpg",
 		:id => 6
 		)
 	Category.create!(
 		:name => "Fashion",
-		:cat_image_url => "assets/fashion-photogur.jpg"
+		:cat_image_url => "assets/fashion-photogur.jpg",
 		:id => 7
 		)
 	Category.create!(
 		:name => "Vintage",
-		:cat_image_url => "assets/vintage-photogur.jpg"
+		:cat_image_url => "assets/vintage-photogur.jpg",
 		:id => 8
 		)
 	Category.create!(
 		:name => "Graphics",
-		:cat_image_url => "assets/graphics-photogur.jpg"
+		:cat_image_url => "assets/graphics-photogur.jpg",
 		:id => 9
 		)
