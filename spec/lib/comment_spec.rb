6a5ea2aa1ee7a7db92eecfb819d5435ca12f4943
Justine Jones <justinejones91@gmail.com>
2014-10-02 21:19:19 -0400

    added the describe blocks

--- spec/lib/comment_spec.rb
+++ spec/lib/comment_spec.rb
@@ -2,4 +2,6 @@ require "spec_helper"
 require "comment"
 
 describe Comment do 
+	it 'is invalid without a description' do
+	end
 end
--- spec/lib/user_spec.rb
+++ spec/lib/user_spec.rb
@@ -2,5 +2,10 @@ require "spec_helper"
 require "user"
 
 describe User do 
-	
+	it 'is invalid without a name'do
+	end
+	it 'is invalid without an email'do
+	end
+	it ' is invalid without a password'do
+	end
 end
